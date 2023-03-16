bb2df61c3edddd804120124b09be845b7fd94d9a
BJ <1228565316@qq.com>
2023-03-16 09:33:31 +0800

    调试spm参数

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JCYMapLibrary",
+            dependencies: [Target.Dependency.byName(name: "arcgis-runtime-ios")],
             path: "JcyMapLibrary/gis",
             resources: [Resource.copy("JcyMapLibrary/Assets.xcassets")]),
     ]
