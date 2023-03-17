eb84092b187dc3ca8014d50c9e33c2187f38be85
BJ <1228565316@qq.com>
2023-03-17 16:20:02 +0800

    提交swift

--- Package.swift
+++ Package.swift
@@ -4,27 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "JCYMapLibrary",
+    name: "JCYMap",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "JCYMapLibrary",
-            targets: ["JCYMapLibrary"]),
+            name: "JCYMap",
+            targets: ["JCYMap"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "arcgis-runtime-ios", url: "https://github.com/Esri/arcgis-runtime-ios.git", .upToNextMajor(from: "100.15.1"))
+        .package(url: "https://github.com/Esri/arcgis-runtime-ios.git", revision: "100.15.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "JCYMapLibrary",
-            dependencies: [.productItem(name: "ArcGIS", package: "arcgis-runtime-ios", moduleAliases: nil, condition: nil)],
-            path: "JcyMapLibrary/gis",
-            resources: [Resource.copy("JcyMapLibrary/Assets.xcassets")]),
+        .binaryTarget(name: "JCYMap",
+                      path: "JcyMapFramework.xcframework"),
     ]
 )
