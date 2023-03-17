2a421e8ab918d8d78af8ecd413e11eb738c51845
BJ <1228565316@qq.com>
2023-03-17 17:12:02 +0800

    更新spm文件

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "JCYMap",
-            targets: ["JCYMap"]),
+            targets: ["JcyMapFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "JCYMap",
+        .binaryTarget(name: "JcyMapFramework",
                       path: "JcyMapFramework.xcframework"),
     ]
 )
