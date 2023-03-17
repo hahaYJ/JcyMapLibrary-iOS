2f11168724b971e3546fbcb575c9e6a985ef0b31
Jason <jiangx.jason@gmail.com>
2023-03-17 10:00:24 +0800

    规整依赖、脚本

--- generate-map-xcframework.sh
+++ generate-map-xcframework.sh
@@ -2,15 +2,15 @@
 rm -rf ./JcyMapFramework.xcframework
 
 #构建模拟器
-#xcodebuild archive \
-#    -project ./JcyMapLibrary-iOS.xcodeproj \
-#    -scheme JcyMapFramework \
-#    -destination "generic/platform=iOS Simulator" \
-#    -sdk iphonesimulator \
-#    -configuration Release \
-#    -archivePath ./build/iphonesimulator \
-#    SKIP_INSTALL=NO \
-#    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive \
+    -project ./JcyMapLibrary-iOS.xcodeproj \
+    -scheme JcyMapFramework \
+    -destination "generic/platform=iOS Simulator" \
+    -sdk iphonesimulator \
+    -configuration Release \
+    -archivePath ./build/iphonesimulator \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 #构建真机
 xcodebuild archive \
@@ -23,11 +23,11 @@ xcodebuild archive \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-##构建xcframework
-#xcodebuild -create-xcframework \
-#    -framework ./build/iphone.xcarchive/Products/Library/Frameworks/JcyMapLibrary.framework \
-#    -framework ./build/iphonesimulator.xcarchive/Products/Library/Frameworks/JcyMapLibrary.framework \
-#    -output ./JcyMapFramework.xcframework
+#构建xcframework
+xcodebuild -create-xcframework \
+    -framework ./build/iphone.xcarchive/Products/Library/Frameworks/JcyMapFramework.framework \
+    -framework ./build/iphonesimulator.xcarchive/Products/Library/Frameworks/JcyMapFramework.framework \
+    -output ./JcyMapFramework.xcframework
 
 #删除构建中间产物
-#rm -rf ./build
+rm -rf ./build
