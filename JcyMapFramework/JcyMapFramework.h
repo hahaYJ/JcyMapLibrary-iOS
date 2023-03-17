e4febdca2065a33d84ea828eb6ce159c95f8aff8
BJ <1228565316@qq.com>
2023-03-17 09:41:47 +0800

    调试framework

---
+++ JcyMapFramework/JcyMapFramework.h
@@ -0,0 +1,18 @@
+//
+//  JcyMapFramework.h
+//  JcyMapFramework
+//
+//  Created by 杨捷 on 2023/3/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JcyMapFramework.
+FOUNDATION_EXPORT double JcyMapFrameworkVersionNumber;
+
+//! Project version string for JcyMapFramework.
+FOUNDATION_EXPORT const unsigned char JcyMapFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JcyMapFramework/PublicHeader.h>
+
+
--- generate-map-xcframework.sh
+++ generate-map-xcframework.sh
@@ -1,21 +1,21 @@
 #删除旧的xcframework
-rm -rf ./JcyMapLibrary.xcframework
+rm -rf ./JcyMapFramework.xcframework
 
 #构建模拟器
-xcodebuild archive \
-    -project ./JcyMapLibrary-iOS.xcodeproj \
-    -scheme JcyMapLibrary \
-    -destination "generic/platform=iOS Simulator" \
-    -sdk iphonesimulator \
-    -configuration Release \
-    -archivePath ./build/iphonesimulator \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+#xcodebuild archive \
+#    -project ./JcyMapLibrary-iOS.xcodeproj \
+#    -scheme JcyMapFramework \
+#    -destination "generic/platform=iOS Simulator" \
+#    -sdk iphonesimulator \
+#    -configuration Release \
+#    -archivePath ./build/iphonesimulator \
+#    SKIP_INSTALL=NO \
+#    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 #构建真机
 xcodebuild archive \
     -project ./JcyMapLibrary-iOS.xcodeproj \
-    -scheme JcyMapLibrary \
+    -scheme JcyMapFramework \
     -destination "generic/platform=iOS" \
     -sdk iphoneos \
     -configuration Release \
@@ -23,11 +23,11 @@ xcodebuild archive \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-#构建xcframework
-xcodebuild -create-xcframework \
-    -framework ./build/iphone.xcarchive/Products/Library/Frameworks/JcyMapLibrary.framework \
-    -framework ./build/iphonesimulator.xcarchive/Products/Library/Frameworks/JcyMapLibrary.framework \
-    -output ./JcyMapLibrary.xcframework
+##构建xcframework
+#xcodebuild -create-xcframework \
+#    -framework ./build/iphone.xcarchive/Products/Library/Frameworks/JcyMapLibrary.framework \
+#    -framework ./build/iphonesimulator.xcarchive/Products/Library/Frameworks/JcyMapLibrary.framework \
+#    -output ./JcyMapFramework.xcframework
 
 #删除构建中间产物
-rm -rf ./build
+#rm -rf ./build
